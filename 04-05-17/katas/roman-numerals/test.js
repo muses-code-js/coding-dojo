0d7ce6ae0d427c7b5d157b19dc193dba57d7775a
Rodolfo Pereira <rodolfo@pragma.team>
2017-05-04 17:11:04 +1000

    katas for 04-05-17

---
+++ 04-05-17/katas/roman-numerals/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Roman Numerals', () => {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(expected, actual);
+  });
+
+});
