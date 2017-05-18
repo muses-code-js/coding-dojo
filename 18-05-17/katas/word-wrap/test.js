480ed3ef9fcff6bc0b755767104eb0aa579ee1b9
Rodolfo Pereira <rod.lps@hotmail.com>
2017-05-18 14:43:25 +1000

    Add Katas for 18-05-17 (#5)

---
+++ 18-05-17/katas/word-wrap/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Word Wrap', () => {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(expected, actual);
+  });
+
+});
