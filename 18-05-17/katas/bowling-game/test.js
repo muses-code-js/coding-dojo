5091b2a6e7421b93cea57f9b89de39f717215064
Rodolfo Pereira <rod.lps@hotmail.com>
2017-05-19 11:53:01 +1000

    Add Bowling Game Kata - 18-05-17 (#8)

--- 18-05-17/katas/bowling-game/test.js
+++ 18-05-17/katas/bowling-game/test.js
@@ -1,12 +1,53 @@
 const assert = require('assert');
+const BowlingGame = require('./');
+
 
 describe('Bowling Game', function() {
 
-  it('works', () => {
-    const expected = true;
-    const actual = false;
+  it('first two rolls get 0, should return 0', () => {
+    const bowlingGame = new BowlingGame();
+    bowlingGame.roll(0);
+    bowlingGame.roll(0);
+    const actual = bowlingGame.score();
 
-    assert.equal(expected, actual);
+    assert.equal(actual, 0);
   });
 
+  it('first two rolls get 1, should return 1', () => {
+    const bowlingGame = new BowlingGame();
+    bowlingGame.roll(0);
+    bowlingGame.roll(1);
+    const actual = bowlingGame.score();
+
+    assert.equal(actual, 1);
+  });
+
+  it('first two roll get one each score, should return 2', () => {
+    const bowlingGame = new BowlingGame();
+    bowlingGame.roll(1);
+    bowlingGame.roll(1);
+    const actual = bowlingGame.score();
+
+    assert.equal(actual, 2);
+  });
+
+  it('strike without further pins', () => {
+    const bowlingGame = new BowlingGame();
+    bowlingGame.roll(10);
+    bowlingGame.roll(0);
+    bowlingGame.roll(0);
+    const actual = bowlingGame.score();
+
+    assert.equal(actual, 10);
+  })
+
+  it('strike plus one pin', () => {
+    const bowlingGame = new BowlingGame();
+    bowlingGame.roll(10);
+    bowlingGame.roll(1);
+    bowlingGame.roll(1);
+    const actual = bowlingGame.score();
+
+    assert.equal(actual, 12);
+  })
 });
