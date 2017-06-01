af68d101be0b117f9475a6f4be26cef3682aebfe
Reena H Rajani <ReenaRajani@users.noreply.github.com>
2017-06-01 16:22:28 +1000

    Kata for June 1 (#10)

---
+++ 01-06-17/katas/bowling-game/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Bowling Game', () => {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(expected, actual);
+  });
+
+});
---
+++ 01-06-17/katas/fizz-buzz/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Fizz Buzz', () => {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(expected, actual);
+  });
+
+});
