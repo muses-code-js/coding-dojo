17659c922986fab1d34748997db70d9b0a3ccfc3
Rodolfo Pereira <rodolfo@pragma.team>
2017-06-02 10:58:49 +1000

    Bowling Game - New team

--- 01-06-17/katas/bowling-game/test.js
+++ 01-06-17/katas/bowling-game/test.js
@@ -1,12 +1,64 @@
 const assert = require('assert');
 
+class BowlingGame {
+  constructor(){
+    this.counter = 0;
+    this.numberofplaying = 0;
+  };
+
+  roll(pins) {
+     this.counter += pins;
+     this.numberofplaying++;
+  };
+
+  get score() {
+    return this.counter;
+  }
+};
+
 describe('Bowling Game', () => {
 
-  it('works', () => {
-    const expected = true;
-    const actual = false;
+  it('keeps score each 1 roll', () => {
+    const game = new BowlingGame();
+    const expected = 3;
+    game.roll(3);
+    const actual = game.score;
 
     assert.equal(expected, actual);
   });
+
+  it('keeps score 2 rolls', () => {
+    const game = new BowlingGame();
+    const expected = 5;
+    game.roll(2);
+    game.roll(3);
+    const actual = game.score;
+
+    assert.equal(actual, expected);
+  });
+
+  it('keeps score for a full game without strike and spare', () => {
+    const game = new BowlingGame();
+    const expected = 40;
+    for (var i = 0; i < 20; i++) {
+      game.roll(2);
+    };
+    const actual = game.score;
+
+    assert.equal(actual, expected);
+  });
+
+  it('Rule of spare', () => {
+
+    const game = new BowlingGame();
+    const expected = 16;
+      game.roll(8);
+      game.roll(2);
+      game.roll(3);
+    const actual = game.score;
+
+    assert.equal(actual, expected);
+  });
+
 
 });
