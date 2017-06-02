fd67dacba918e8e42e7089291d423715ad796557
Rodolfo Pereira <rod.lps@hotmail.com>
2017-06-02 11:26:12 +1000

    Code for Katas 01-06-17 (#11)
    * Bowling Game - New team
    
    * Fizz Buzz
    
    * README.md

--- 01-06-17/katas/fizz-buzz/test.js
+++ 01-06-17/katas/fizz-buzz/test.js
@@ -1,12 +1,62 @@
 const assert = require('assert');
 
-describe('Fizz Buzz', () => {
+fizzbuzz = (number) => {
+  if (number % 15 == 0){
+    return "FizzBuzz"
+  }
+  if (number % 3 == 0){
+   return "Fizz"
+  }
+  if (number % 5 == 0){
+    return "Buzz"
+  }
+  return number
+}
 
-  it('works', () => {
-    const expected = true;
-    const actual = false;
+function fizzbuzzupto(number) {
+  if (number == 1) {
+    for (i = 1; i <= 1; i++) {
+      return [fizzbuzz(i)]
+    }
+  }
+  if (number == 2) {
+    for (i = 1; i <= number; i++) {
+    return [fizzbuzz(i), fizzbuzz(number)]
+    }
+  }
+  if (number == 3) {
+    return [fizzbuzz(1), fizzbuzz(2), fizzbuzz(number)]
+  }
+  if (number == 4) {
+    return [fizzbuzz(1), fizzbuzz(2), fizzbuzz(3), fizzbuzz(number)]
+  }
+}
 
-    assert.equal(expected, actual);
+describe('FizzBuzz', function () {
+  it('returns a number', function () {
+    assert.equal(fizzbuzz(1), 1)
+    assert.equal(fizzbuzz(2), 2)
+    assert.equal(fizzbuzz(4), 4)
+    assert.equal(fizzbuzz(7), 7)
   });
-
+  it('returns a Fizz for multiples of 3', function () {
+    assert.equal(fizzbuzz(3), "Fizz")
+    assert.equal(fizzbuzz(6), "Fizz")
+    assert.equal(fizzbuzz(9), "Fizz")
+  });
+  it('returns a Buzz for multiples of 5', function () {
+    assert.equal(fizzbuzz(5), "Buzz")
+    assert.equal(fizzbuzz(10), "Buzz")
+  });
+  it('returns a FizzBuzz for multiples of 5 and 3', function () {
+    assert.equal(fizzbuzz(15), "FizzBuzz")
+    assert.equal(fizzbuzz(30), "FizzBuzz")
+    assert.equal(fizzbuzz(45), "FizzBuzz")
+  });
+  it('input returns an array of Fizzbuzzes up to number', function() {
+    assert.deepEqual(fizzbuzzupto(1),[1])
+    assert.deepEqual(fizzbuzzupto(2),[1,2])
+    assert.deepEqual(fizzbuzzupto(3),[1,2,"Fizz"])
+    assert.deepEqual(fizzbuzzupto(4),[1,2,"Fizz", 4])
+  });
 });
