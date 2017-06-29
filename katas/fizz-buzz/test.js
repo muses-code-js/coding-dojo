f8b8aad5c6dd11158860ee2aee6918a850ef82da
Rodolfo Pereira <rodolfo@pragma.team>
2017-06-29 18:00:47 +1000

    Adding Fizz Buzz Kata

---
+++ katas/fizz-buzz/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Fizz Buzz Game', function() {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(actual, expected);
+  });
+
+});
