53a9e1f309dd3b935f7d291058c0653990c8bb03
“ReenaRajani” <reenahrajani@gmail.com>
2017-07-27 16:17:43 +1000

    added additional katas

---
+++ katas/number-letter-counts/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Number Letter Counts', function() {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(actual, expected);
+  });
+
+});
---
+++ katas/sum-of-multiples/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Multiple of 3 and 5', function() {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(actual, expected);
+  });
+
+});
