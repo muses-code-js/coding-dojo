62225e94bb430c74ec861817a2e26146c7e4cb99
Rhys <rbrooker105@gmail.com>
2017-07-31 14:25:52 +1000

    refactor scrabble-score/test.js to have only basic test suite

--- katas/scrabble-score/test.js
+++ katas/scrabble-score/test.js
@@ -1,5 +1,6 @@
 function scrabbleScore(word) {
   //your solution
+  return 0;
 }
 
 /* Letter value table
@@ -21,49 +22,6 @@ describe('Scrabble scoring game', () => {
   it('scores an empty word as zero', () => {
     const expected = 0;
     const actual = scrabbleScore('');
-
-    assert.equal(actual, expected);
-  });
-
-  xit('scores a very short simple word',function() {
-    const expected = 1;
-    const actual = scrabbleScore('a');
-
-    assert.equal(actual, expected);
-  });
-
-  xit('scores the word by the number of letters',function() {
-    const expected = 6;
-    const actual = scrabbleScore('street');
-
-    assert.equal(actual, expected);
-  });
-
-  xit('scores letters with high points correctly',function() {
-    const expected = 10;
-    const actual = scrabbleScore('z');
-
-    assert.equal(actual, expected);
-  });
-
-  xit('scores a short word with higher point letters',function() {
-    const expected = 5;
-    const actual = scrabbleScore('yo');
-
-    assert.equal(actual, expected);
-  });
-
-  xit('scores more complicated words with more',function() {
-    const expected = 22;
-    const actual = scrabbleScore('quirky');
-
-    assert.equal(actual, expected);
-  });
-
-  xit('scores case insensitive words',function() {
-    const expected = 41;
-    const actual = scrabbleScore('OXYPHENBUTAZONE');
-
     assert.equal(actual, expected);
   });
 
