44d593d310c51137d527140d87616b19c4306071
Rodolfo Pereira <rod.lps@hotmail.com>
2017-08-10 17:39:00 +1000

    Add Banking Kata (#28)

---
+++ katas/banking-kata/test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('Banking Kata', () => {
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(actual, expected);
+  });
+});
