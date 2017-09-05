d437938e9c2a0af0e8baba8ab1e566b90e4c7b3f
“ReenaRajani” <reenahrajani@gmail.com>
2017-09-05 13:46:32 +1000

    added ceaser shift chipher and palindromes

---
+++ katas/ceasar-shift-cipher/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Ceaser Shift Cipher', () => {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(actual, expected);
+  });
+
+});
