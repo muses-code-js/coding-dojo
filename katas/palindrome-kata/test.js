e407d150aa84f65585266f297370fdd1499881fa
Reena H Rajani <ReenaRajani@users.noreply.github.com>
2017-09-06 10:01:03 +1000

    added ceaser shift chipher and palindromes (#36)

---
+++ katas/palindrome-kata/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+describe('Palindrome', () => {
+
+  it('works', () => {
+    const expected = true;
+    const actual = false;
+
+    assert.equal(actual, expected);
+  });
+
+});
