b6a929dffd880665785ad6d6b31d20ee37b16afc
Carvallegro <carvallegro@users.noreply.github.com>
2018-05-21 23:24:20 +1000

    add Yatzy kata as both refactoring and green field (#58)
    * add Yatzy kata as both refactoring and green field
    
    * Add default code for greenfield
    
    * uppercased the Yatzy function

---
+++ katas/yatzy/index.js
@@ -0,0 +1,3 @@
+function Yatzy(){}
+
+module.exports = yatzy;
---
+++ katas/yatzy/test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert
+
+const Yatzy = require('./index')
+
+describe('Yatzi game', () => {
+    it('should be fixed', () => {
+        assert(false).to.be.true
+    })
+})
