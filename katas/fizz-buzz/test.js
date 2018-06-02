7e2107c1329c5798c3622041ff48f41f6d70b8a7
Carvallegro <carvallegro@users.noreply.github.com>
2018-06-02 11:33:10 +1000

    Revert "Group A: FizzBuzz work" (#60)
    This reverts commit e8be5237f653eb8000ae72916381c81a99f43662.

--- katas/fizz-buzz/test.js
+++ katas/fizz-buzz/test.js
@@ -1,53 +1,12 @@
-var fizzbuzz = require('./index').fizzbuzz;
-var printLines = require('./index').printLines;
+const assert = require('assert');
 
-var assert = require('chai').assert;
+describe('Fizz Buzz Game', function() {
 
-describe('Example test', function() {
-
-  it('does works', () => {
+  it('works', () => {
     const expected = true;
-    const actual = true;
+    const actual = false;
 
     assert.equal(actual, expected);
   });
-});
-
-describe('Fizz Buzz Game', function() {
- 
-  // Put tests here
-  it('1 should return 1', () => {
-    assert.equal(fizzbuzz(1), 1);
-  })
-
-  it('should return fizz divisible by 3', () => {
-    assert.equal(fizzbuzz(3), 'fizz');
-    assert.equal(fizzbuzz(6), 'fizz');
-
-  })
-
-  it('should return buzz divisible by 5', () => {
-    assert.equal(fizzbuzz(5), 'buzz');
-    assert.equal(fizzbuzz(20), 'buzz');
-
-  })
-  
-  it('should return fizzbuzz if divisible by 15', () => {
-    assert.equal(fizzbuzz(15), 'fizzbuzz');
-    assert.equal(fizzbuzz(45), 'fizzbuzz');
-  })
-  
-  it('should return null if input not a number', () => {
-    assert.equal(fizzbuzz('yes'), null);
-  })
 
 });
-
-describe('Line Printer', function() {
-
-  it('Returns an array', () => {
-    const actual = printLines(0);
-    const expected = '';
-    assert.strictEqual(actual, expected);
-  })
-}) 
--- katas/yatzy/test.js
+++ katas/yatzy/test.js
@@ -1,61 +1,9 @@
-const expect = require('chai').expect
+const assert = require('chai').assert
 
 const Yatzy = require('./index')
 
 describe('Yatzi game', () => {
-    describe('constructor', () => {
-        it('Should throw an exception when the parameter is not an array', () => {
-            expect(() => { new Yatzy(1) }).to.throw('The parameter should be an array of 5 integers between 1 and 6.')
-        })
-        it('Should throw error when the length of the input array is not equal to 5', () => {
-            expect(() => { new Yatzy([1, 2, 3, 4]) }).to.throw('The parameter should be an array of 5 integers between 1 and 6.')
-        })
-        it('Should throw an error if arrays has not integers', () => {
-            expect(() => { new Yatzy([1, 2, 3, "4", 5]) }).to.throw('The parameter should be an array of 5 integers between 1 and 6.')
-        })
-        it('Should throw an error if values are not between 1 and 6.', () => {
-            expect(() => { new Yatzy([1, 2, 3, 20, 5]) }).to.throw('The parameter should be an array of 5 integers between 1 and 6.')
-        })
-        it('Should create an object with the same array values that is provided.', () => {
-            const acceptableRoll = new Yatzy([1, 2, 3, 4, 5])
-            expect(acceptableRoll.diceRolls).to.be.eql([1, 2, 3, 4, 5])
-        })
-    })
-    describe('Chance', () => {
-        it('Should return the sum of all values in the array', () => {
-            const acceptableRoll = new Yatzy([1, 2, 3, 4, 5])
-            expect(acceptableRoll.chance()).to.be.equal(15)
-        })
-    })
-    describe('Yatzi', () => {
-        it('Should return 0 if array has different values', () => {
-            const acceptableRoll = new Yatzy([1, 2, 3, 4, 5])
-            expect(acceptableRoll.yatzy()).to.be.equal(0)
-        })
-        it('Should return the sum of all integers of an array if the array has the same values', () => {
-            const acceptableRoll = new Yatzy([1, 1, 1, 1, 1])
-            expect(acceptableRoll.yatzy()).to.be.equal(5)
-        })
-    })
-    describe('Number', () => {
-        it('Should return 0 if the number doesnt exist in the array', () => {
-            const acceptableRoll = new Yatzy([2, 2, 3, 4, 5])
-            expect(acceptableRoll.number(1)).to.be.equal(0)
-        })
-        it('Should return the sum of the numbers in the array matching the value', () => {
-            const acceptableRoll = new Yatzy([2, 2, 3, 4, 5])
-            expect(acceptableRoll.number(2)).to.be.equal(4)
-        })
-    })
-    describe('Pair', () => {
-        it('Should return 0 if there is no pairs in the array', () => {
-            const acceptableRoll = new Yatzy([1, 2, 3, 4, 5])
-            expect(acceptableRoll.pair()).to.be.equal(0)
-        })
-        it('Should return some of the highest pair', () => {
-            const acceptableRoll = new Yatzy([2, 2, 3, 3, 5])
-            expect(acceptableRoll.pair()).to.be.equal(6)
-        })
+    it('should be fixed', () => {
+        assert(false).to.be.true
     })
 })
-
