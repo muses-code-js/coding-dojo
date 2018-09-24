d1102b746daf2f476da509d3d562d4064f974f6b
Anwesha Chatterjee <anwesha.chatterjee@rea-group.com.au>
2018-09-24 20:55:39 +1000

    solutions upto pair

--- katas/yatzy/index.js
+++ katas/yatzy/index.js
@@ -1,3 +1,25 @@
-function Yatzy(){}
+const _ = require('lodash');
 
-module.exports = yatzy;
+class Yatzy {
+    constructor(roll) {
+        this.roll = roll;
+    }
+    chance() {
+        return _.sum(this.roll);
+    }
+    yatzy() {
+        return _.uniq(this.roll).length === 1 ? 50 : 0;
+    }
+    sum(num) {
+        return _.reduce(this.roll, function(sum, n) {
+          return n === num ? sum + n : sum;
+        }, 0);
+    }
+    pair() {
+        return _.filter(this.roll, (n) => {
+            return n === _.max(this.roll);
+        }).length > 1 ? _.max(this.roll) * 2 : 0;
+    }
+}
+
+module.exports = Yatzy;
--- katas/yatzy/test.js
+++ katas/yatzy/test.js
@@ -1,9 +1,31 @@
-const assert = require('chai').assert
+const expect = require("chai").expect;
 
-const Yatzy = require('./index')
+const Yatzy = require("./index");
 
-describe('Yatzi game', () => {
-    it('should be fixed', () => {
-        assert(false).to.be.true
-    })
-})
+describe("Yatzy game", () => {
+    it("Chance should return sum of roll", () => {
+        chanceRoll = new Yatzy([1, 2, 3, 4, 5]);
+        expect(chanceRoll.chance()).to.be.equal(15);
+    });
+    it("Yatzy should score 50 points", () => {
+        yatzyRoll = new Yatzy([1,1,1,1,1]);
+        expect(yatzyRoll.yatzy()).to.be.equal(50)
+    });
+    it("Non yatzy should score 0 points", () => {
+        nonYatzyRoll = new Yatzy([2,1,1,1,1]);
+        expect(nonYatzyRoll.yatzy()).to.be.equal(0)
+    });
+    it("sum of non unique numbers should be returned", () => {
+        expect( new Yatzy([1,2,1,1,1]).sum(1)).to.be.equal(4);
+        expect( new Yatzy([2,2,1,1,1]).sum(2)).to.be.equal(4);
+        expect( new Yatzy([2,2,1,1,2]).sum(2)).to.be.equal(6);
+    });
+    it("Pair should score sum of highest matching dice", () => {
+        pairRoll = new Yatzy([1,2,3,4,4]);
+        expect(pairRoll.pair()).to.be.equal(8);
+    });
+    it("Pair should score 0 when no matching dice", () => {
+        pairRoll = new Yatzy([1,2,3,4,5]);
+        expect(pairRoll.pair()).to.be.equal(0);
+    });
+});
